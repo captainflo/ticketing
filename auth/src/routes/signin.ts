0360f003a59a1486197434ebcad1b3dbad66296c
Florian Lahitte <lahitte.florian@gmail.com>
2020-09-25 13:48:56 -0400

    Signin Signout middleware request amd error type...

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,9 +1,58 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
+import { validationRequest } from '../middlewares/validate-request';
+import { User } from '../models/user';
+import { BadRequestError } from '../errors/bad-request-error';
+import { Password } from '../services/password';
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 
-router.post('/api/users/signin', (req, res) => {
-  res.send('hi there');
-});
+router.post(
+  '/api/users/signin',
+  [
+    body('email').isEmail().withMessage('Email must be valid'),
+    body('password')
+      .trim()
+      .notEmpty()
+      .withMessage('You must supply a password'),
+  ],
+  validationRequest,
+  async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+
+    const existinUser = await User.findOne({ email });
+    if (!existinUser) {
+      throw new BadRequestError('Invalid credentials');
+    }
+
+    const passwordMatch = await Password.compare(
+      existinUser.password,
+      password
+    );
+
+    if (!passwordMatch) {
+      throw new BadRequestError('Invalid credentials');
+    }
+
+    // Generate JWT
+    const userJwt = jwt.sign(
+      {
+        id: existinUser.id,
+        email: existinUser.email,
+      },
+      // ! make the exclamation mark tell to typescript
+      // we are sure at 100% the variable is defined
+      process.env.JWT_KEY!
+    );
+
+    // Store it on session object
+    req.session = {
+      jwt: userJwt,
+    };
+
+    res.status(200).send(existinUser);
+  }
+);
 
 export { router as signinRouter };
