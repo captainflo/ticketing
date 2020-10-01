96972ea49e83a893c0ba952016a06aea1f94d4a5
Florian Lahitte <lahitte.florian@gmail.com>
2020-10-01 15:22:25 -0400

    passing cookie and specifing the host for the request server with getinitalprops

--- client/pages/index.js
+++ client/pages/index.js
@@ -5,9 +5,20 @@ const LandingPage = ({ currentUser }) => {
   return <h1>Landing Page</h1>;
 };
 
-LandingPage.getInitialProps = async () => {
-  const response = await axios.get('/api/users/currentuser');
-  return response.data;
+LandingPage.getInitialProps = async ({ req }) => {
+  if (typeof window === 'undefined') {
+    const response = await axios.get(
+      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
+      {
+        headers: req.headers,
+      }
+    );
+    return response.data;
+    // We are on the server
+    //  Request should be made to http://ingress-nginx.ingress-nginx...
+  } else {
+    const response = await axios.get('/api/users/currentuser');
+    return response.data;
+  }
 };
-
 export default LandingPage;
