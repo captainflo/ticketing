b16c5da3116a161ae3433d04376a6a3401509464
Florian Lahitte <lahitte.florian@gmail.com>
2020-10-20 10:39:55 -0400

    add publisher and listner to orders

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -1,10 +1,12 @@
 import mongoose from 'mongoose';
 import { app } from './app';
 import { natsWrapper } from './nats-wrapper';
+import { TicketCreatedListener } from './events/listeners/ticket-created-listener';
+import { TicketUpdatedListener } from './events/listeners/ticket-updated-listener';
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
-    throw new Error('No key JWT');
+    throw new Error('JWT_KEY must be defined');
   }
   if (!process.env.MONGO_URI) {
     throw new Error('MONGO_URI must be defined');
@@ -18,13 +20,13 @@ const start = async () => {
   if (!process.env.NATS_CLUSTER_ID) {
     throw new Error('NATS_CLUSTER_ID must be defined');
   }
+
   try {
     await natsWrapper.connect(
       process.env.NATS_CLUSTER_ID,
       process.env.NATS_CLIENT_ID,
       process.env.NATS_URL
     );
-
     natsWrapper.client.on('close', () => {
       console.log('NATS connection closed!');
       process.exit();
@@ -32,18 +34,21 @@ const start = async () => {
     process.on('SIGINT', () => natsWrapper.client.close());
     process.on('SIGTERM', () => natsWrapper.client.close());
 
+    new TicketCreatedListener(natsWrapper.client).listen();
+    new TicketUpdatedListener(natsWrapper.client).listen();
+
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true,
     });
-    console.log('connected to mongoDb Orders ');
+    console.log('Connected to MongoDb');
   } catch (err) {
-    console.log(err);
+    console.error(err);
   }
 
   app.listen(3000, () => {
-    console.log('Listening on port 3000');
+    console.log('Listening on port 3000!!!!!!!!');
   });
 };
 
--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -5,7 +5,8 @@ import {
   NotAuthorizedError,
 } from '@fldevtickets/common';
 import { Order, OrderStatus } from '../models/orders';
-
+import {OrderCancelledPublisher} from '../events/publishers/order-cancelled-publisher'
+import {natsWrapper} from '../nats-wrapper'
 const router = express.Router();
 
 router.delete(
@@ -14,7 +15,7 @@ router.delete(
   async (req: Request, res: Response) => {
     const { orderId } = req.params;
 
-    const order = await Order.findById(orderId);
+    const order = await Order.findById(orderId).populate('ticket')
 
     if (!order) {
       throw new NotFoundError();
@@ -26,7 +27,12 @@ router.delete(
     await order.save();
 
     // publishing an event saying this was cancelled!
-
+    new OrderCancelledPublisher(natsWrapper.client).publish({
+      id: order.id,
+      ticket: {
+        id: order.ticket.id,
+      }
+    })
     res.status(204).send(order);
   }
 );
