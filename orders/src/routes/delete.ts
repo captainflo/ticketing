7f782c156c05f585192babbf77ea7b208c8188ec
Florian Lahitte <lahitte.florian@gmail.com>
2020-10-21 10:35:37 -0400

    add event versioning in common ticket and order

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -29,6 +29,7 @@ router.delete(
     // publishing an event saying this was cancelled!
     new OrderCancelledPublisher(natsWrapper.client).publish({
       id: order.id,
+      version: order.__v!,
       ticket: {
         id: order.ticket.id,
       }
